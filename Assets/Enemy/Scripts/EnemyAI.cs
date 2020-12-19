10f18665b1ed8ce8804de15c50d3a9d05b9edb9f
atul219 <atulyadav219@gmail.com>
2020-12-19 17:51:02 +0530

    first commit

---
+++ Assets/Enemy/Scripts/EnemyAI.cs
@@ -0,0 +1,91 @@
+﻿//using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] Transform player;
+    [SerializeField] float chaseRange = 10f;
+    [SerializeField] float turnSpeed = 5f;
+
+    NavMeshAgent navMeshAgent;
+
+    float distanceToPlayer = Mathf.Infinity;
+
+    bool isProvked = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        distanceToPlayer = Vector3.Distance(player.position, transform.position);
+
+        if (isProvked)
+        {
+            EngagePlayer();
+        }
+
+        else if (distanceToPlayer <= chaseRange)
+        {
+            // if player is visible
+            isProvked = true;
+        }
+        
+        
+    }
+
+    
+    private void EngagePlayer()
+    {
+        FacePlayer();
+
+        if (distanceToPlayer >= navMeshAgent.stoppingDistance)
+        {
+            ChasePlayer();
+        }
+
+        if (distanceToPlayer <= navMeshAgent.stoppingDistance)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+
+        GetComponent<Animator>().SetBool("attack", false);
+        GetComponent<Animator>().SetTrigger("move");
+        navMeshAgent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        GetComponent<Animator>().SetBool("attack", true);
+    }
+
+    private void FacePlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+
+    
+}
---
+++ Assets/Enemy/Scripts/EnemyAttack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    PlayerHealth player;
+    [SerializeField] float damage = 40f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerHealth>();
+    }
+
+    public void AttackHitEvent()
+    {
+        if (player == null) return;
+        player.TakeDamage(damage);
+    }
+}
